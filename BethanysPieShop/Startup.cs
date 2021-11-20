d53bee883069d4949e02b7af585c3f193399ce78
Ivan MSI <arigatory@gmail.com>
2021-11-21 00:29:27 +0300

    add contact categories cart

--- BethanysPieShop/Startup.cs
+++ BethanysPieShop/Startup.cs
@@ -30,6 +30,10 @@ namespace BethanysPieShop
             services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddScoped<IPieRepository, PieRepository>();
             services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<ShoppingCart>(sp => ShoppingCart.GetCart(sp));
+            services.AddHttpContextAccessor();
+            services.AddSession();
+
             services.AddControllersWithViews();
         }
 
@@ -42,6 +46,7 @@ namespace BethanysPieShop
             }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+            app.UseSession();
             
             app.UseRouting();
 
